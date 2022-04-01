3526a5c4c387738607f69a363484d78127308d11
chemacenturion <centurion.philadelphia@gmail.com>
2022-03-31 21:07:07 -0400

    open modal functionality

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
 // Start writing JavaScript here!
+const modalButton = document.querySelector('.jsModalButton')
+
+modalButton.addEventListener('click', event => {
+    document.body.classList.add('modal-is-open')
+})
