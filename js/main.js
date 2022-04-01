6438d18746000badd2c7e09597332af20a3fdac1
chemacenturion <centurion.philadelphia@gmail.com>
2022-03-31 21:12:36 -0400

    modal close functionality

--- js/main.js
+++ js/main.js
@@ -4,3 +4,8 @@ const modalButton = document.querySelector('.jsModalButton')
 modalButton.addEventListener('click', event => {
     document.body.classList.add('modal-is-open')
 })
+
+const closeButton = document.querySelector('.jsModalClose')
+closeButton.addEventListener('click', event => {
+    document.body.classList.remove('modal-is-open')
+})
